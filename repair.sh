4d6dcc2ec7a88bf7827aa5d36c4732e7abe14200
VanzzzTOT <113664657+VanzzzTOT@users.noreply.github.com>
2024-09-16 00:07:10 +0700

    Create repair.sh

---
+++ repair.sh
@@ -0,0 +1,39 @@
+if (( $EUID != 0 )); then
+    echo "Please run as root"
+    exit
+fi
+
+repairPanel(){
+    cd /var/www/pterodactyl
+
+    php artisan down
+
+    rm -r /var/www/pterodactyl/resources
+
+    curl -L https://github.com/pterodactyl/panel/releases/latest/download/panel.tar.gz | tar -xzv
+
+    chmod -R 755 storage/* bootstrap/cache
+
+    composer install --no-dev --optimize-autoloader
+
+    php artisan view:clear
+
+    php artisan config:clear
+
+    php artisan migrate --seed --force
+
+    chown -R www-data:www-data /var/www/pterodactyl/*
+
+    php artisan queue:restart
+
+    php artisan up
+}
+
+while true; do
+    read -p "Untuk mengunistall thema ketik [y/n] apakah anda menyetujui nya?" yn
+    case $yn in
+        [Yy]* ) repairPanel; break;;
+        [Nn]* ) exit;;
+        * ) echo "Ketik Y Jika Ingin Uninstall Thema Jika tidak Ketik N.";;
+    esac
+done
